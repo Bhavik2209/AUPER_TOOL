3216a15936c3a3b1b63e70eb2494e90eb34f7c79
Bhavik2209 <bhavikrohit22@gmail.com>
2024-10-08 14:14:24 +0530

    version 2

--- b.py
+++ b.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import PyPDF2
+import os
+from gtts import gTTS
 import pyttsx3
 import google.generativeai as genai
 import re
+import tempfile
 from streamlit_extras.stylable_container import stylable_container
 from streamlit_option_menu import option_menu
 import time
 
 try:
-    GOOGLE_API_KEY = st.secrets['default']['GOOGLE_API_KEY']
+    GOOGLE_API_KEY = st.secrets["defalut"]["GOOGLE_API_KEY"]
 except KeyError:
     st.error("GOOGLE_API_KEY not found in Streamlit secrets. Please set it up.")
     st.stop()
